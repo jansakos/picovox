eb37ec8162562b0596b95b57e3fec66b36602892
Jan Samek <xsamek1@fi.muni.cz>
2025-09-19 01:05:13 +0200

    Added definitions for I2S library

--- picoLPTsound.c
+++ picoLPTsound.c
@@ -1,13 +1,28 @@
+// Definitions for I2S library
+#define PICO_AUDIO_I2S_CLOCK_PINS_SWAPPED 0
+#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 26
+#define PICO_AUDIO_I2S_DATA_PIN 28
+#define PICO_AUDIO_PIO 0
+#define PICO_AUDIO_DMA_IRQ 1
+#define SAMPLES_PER_BUFFER 512
+#define NUM_BUFFERS 8
+
+// Definitions for PIO programs
+#define LPT_STROBE_PIN 0
+#define LPT_DATA_BASE 1
+#define SAMPLE_RATE 44100
+
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "pio/covox.pio.h"
+#include "pico/audio_i2s.h"
 
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    
 }
