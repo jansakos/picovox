4372f1df87db423230b93f53cb09f0297ac3725b
Jan Samek <vosudo@gmail.com>
2026-01-04 03:22:17 +0100

    Fixed Stereo pin inits

--- stereo.c
+++ stereo.c
@@ -47,6 +47,8 @@ bool load_stereo(Device *self) {
         pio_gpio_init(sound_left_pio, i);
     }
 
+    pio_gpio_init(sound_left_pio, LPT_AUTOFEED_PIN);
+
     pio_sm_set_consecutive_pindirs(sound_left_pio, sound_left_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
 
     if (pio_sm_init(sound_left_pio, sound_left_sm, sound_left_offset, &used_config_left) < 0) {
@@ -62,6 +64,8 @@ bool load_stereo(Device *self) {
     for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Sets pins to use PIO
         pio_gpio_init(sound_right_pio, i);
     }
+
+    pio_gpio_init(sound_right_pio, LPT_STROBE_PIN);
 
     pio_sm_set_consecutive_pindirs(sound_right_pio, sound_right_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
 
@@ -103,18 +107,17 @@ bool unload_stereo(Device *self) {
         gpio_deinit(i);
     }
     gpio_deinit(LPT_STROBE_PIN);
+    gpio_deinit(LPT_AUTOFEED_PIN);
     gpio_deinit(LPT_BUSY_PIN);
     return true;
 }
 
 size_t generate_stereo(Device *self, int16_t *left_sample, int16_t *right_sample) {
-    /*while (pio_sm_is_rx_fifo_empty(sound_left_pio, sound_left_sm) || pio_sm_is_rx_fifo_empty(sound_right_pio, sound_right_sm)) {
+    while (pio_sm_is_rx_fifo_empty(sound_left_pio, sound_left_sm) ^ pio_sm_is_rx_fifo_empty(sound_right_pio, sound_right_sm)) {
         tight_loop_contents();
-    }*/
+    }
     if (!pio_sm_is_rx_fifo_empty(sound_left_pio, sound_left_sm)) {
         *left_sample = (((pio_sm_get(sound_left_pio, sound_left_sm) >> 24) & 0xFF) - 128) << 8;
-    }
-    if (!pio_sm_is_rx_fifo_empty(sound_right_pio, sound_right_sm)) {
         *right_sample = (((pio_sm_get(sound_right_pio, sound_right_sm) >> 24) & 0xFF) - 128) << 8;
     }
     return 0;
