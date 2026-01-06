f07d4d51dfb9ebd1231e8bdde5f5807a7cfcea3b
Jan Samek <vosudo@gmail.com>
2026-01-06 02:42:45 +0100

    Added header of CMS into square_c

--- square/square_c.h
+++ square/square_c.h
@@ -51,6 +51,44 @@ int32_t tandy_get_sample(tandy_t *tandy);
  */
 void tandy_destroy(tandy_t *tandy);
 
+/**
+ * CMS part (two chips SAA-1099)
+ */
+
+typedef struct cms_t cms_t;
+
+/**
+ * @brief Creates a new CMS sound device.
+ * 
+ * @return pointer to the created (and loaded) device.
+ */
+cms_t *cms_create(void);
+
+/**
+ * @brief Sends new data into CMS.
+ * 
+ * @param cms is a pointer to the loaded CMS device.
+ * @param address is an address where data should be placed.
+ * @param data is raw data, input to the CMS device.
+ */
+void cms_write(cms_t *cms, uint32_t address, uint8_t data);
+
+/**
+ * @brief Gets a sample from the CMS device.
+ * 
+ * @param cms is a pointer to the loaded CMS device.
+ * @param left is a pointer where left sample should be stored.
+ * @param right is a pointer where right sample should be stored.
+ */
+void cms_get_sample(cms_t *cms, int16_t *left, int16_t *right);
+
+/**
+ * @brief Destroys (unloads) given CMS device.
+ * 
+ * @param cms is a pointer to the loaded CMS device.
+ */
+void cms_destroy(cms_t *cms);
+
 #ifdef __cplusplus
 }
 #endif
