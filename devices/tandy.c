cda300f7abe695f365bbf531d83068f407efc6e2
Jan Samek <vosudo@gmail.com>
2026-01-07 01:12:48 +0100

    Cleaned the structure of the code

--- devices/tandy.c
+++ devices/tandy.c
@@ -14,11 +14,16 @@
 #include "tandy.pio.h"
 
 #define TND_RINGBUFFER_SIZE 2048
+#define TND_DETECTION_FREQ_HZ 4000000
 
 // Variables for PIO - each device simulated has its own
-static PIO used_pio;
-static int8_t used_sm;
-static int used_offset;
+static PIO sound_pio;
+static int8_t sound_sm;
+static int sound_offset;
+
+static PIO detection_pio;
+static int8_t detection_sm;
+static int detection_offset;
 
 // Killswitch for core1
 static volatile bool stop_core1 = false;
@@ -26,11 +31,11 @@ static volatile bool stop_core1 = false;
 static bool sample_used = false;
 
 static void load_new_instruction(tandy_t *device) {
-    if (pio_sm_is_rx_fifo_empty(used_pio, used_sm)) {
+    if (pio_sm_is_rx_fifo_empty(sound_pio, sound_sm)) {
         return;
     }
 
-    tandy_write(device, pio_sm_get(used_pio, used_sm) >> 24);
+    tandy_write(device, pio_sm_get(sound_pio, sound_sm) >> 24);
 }
 
 static void reset_chip(tandy_t **device) {
@@ -47,7 +52,7 @@ static void core1_operation(void) {
     int16_t current_sample = 0;
 
     while (!stop_core1) {
-        while ((!pio_sm_is_rx_fifo_empty(used_pio, used_sm))) {
+        while ((!pio_sm_is_rx_fifo_empty(sound_pio, sound_sm))) {
             load_new_instruction(device);
         }
         current_sample = tandy_get_sample(device);
@@ -63,46 +68,66 @@ bool load_tandy(Device *self) {
 
     ringbuffer_init(TND_RINGBUFFER_SIZE);
 
-    used_offset = pio_manager_load(&used_pio, &used_sm, &tandy_program);
-    if (used_offset < 0) {
+    sound_offset = pio_manager_load(&sound_pio, &sound_sm, &tandy_sound_program);
+    if (sound_offset < 0) {
         return false;
     }
 
-    pio_sm_config used_config = tandy_program_get_default_config(used_offset);
-    sm_config_set_in_pins(&used_config, LPT_BASE_PIN);
-    sm_config_set_fifo_join(&used_config, PIO_FIFO_JOIN_RX);
+    detection_offset = pio_manager_load(&detection_pio, &detection_sm, &tandy_detection_program);
+    if (detection_offset < 0) {
+        return false;
+    }
+
+    pio_sm_config sound_config = tandy_sound_program_get_default_config(sound_offset);
+    sm_config_set_in_pins(&sound_config, LPT_BASE_PIN);
+    sm_config_set_fifo_join(&sound_config, PIO_FIFO_JOIN_RX);
 
     for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Sets pins to use PIO
-        pio_gpio_init(used_pio, i);
+        pio_gpio_init(sound_pio, i);
     }
 
-    pio_gpio_init(used_pio, LPT_STROBE_PIN);
-    pio_gpio_init(used_pio, LPT_INIT_PIN);
+    pio_gpio_init(sound_pio, LPT_STROBE_PIN);
+    pio_gpio_init(sound_pio, LPT_INIT_PIN);
 
     gpio_init(LPT_SELIN_PIN);
-    gpio_init(LPT_ACK_PIN);
-    gpio_set_dir(LPT_ACK_PIN, true);
 
-    pio_sm_set_consecutive_pindirs(used_pio, used_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
+    pio_sm_set_consecutive_pindirs(sound_pio, sound_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
 
-    if (pio_sm_init(used_pio, used_sm, used_offset, &used_config) < 0) {
+    if (pio_sm_init(sound_pio, sound_sm, sound_offset, &sound_config) < 0) {
         return false;
     }
 
-    pio_sm_set_enabled(used_pio, used_sm, true);
+    pio_sm_set_enabled(sound_pio, sound_sm, true);
+
+    pio_sm_config detection_config = tandy_detection_program_get_default_config(detection_offset);
+    sm_config_set_set_pin_base(&detection_config, LPT_ACK_PIN);
+    sm_config_set_clkdiv(&detection_config, clock_get_hz(clk_sys) / TND_DETECTION_FREQ_HZ);
+
+    pio_gpio_init(detection_pio, LPT_STROBE_PIN);
+    pio_gpio_init(detection_pio, LPT_INIT_PIN);
+    pio_gpio_init(detection_pio, LPT_ACK_PIN);
+
+    pio_sm_set_consecutive_pindirs(detection_pio, detection_sm, LPT_ACK_PIN, 1, true); // Sets pins in PIO to be inputs
+
+    if (pio_sm_init(detection_pio, detection_sm, detection_offset, &detection_config) < 0) {
+        return false;
+    }
+
+    pio_sm_set_enabled(detection_pio, detection_sm, true);
 
     stop_core1 = false;
     multicore_reset_core1();
     multicore_launch_core1(core1_operation);
-
-    gpio_put(LPT_ACK_PIN, true);
     return true;
 }
 
 bool unload_tandy(Device *self) {
     stop_core1 = true;
-    pio_sm_set_enabled(used_pio, used_sm, false);
-    pio_manager_unload(used_pio, used_sm, used_offset, &tandy_program);
+    pio_sm_set_enabled(sound_pio, sound_sm, false);
+    pio_manager_unload(sound_pio, sound_sm, sound_offset, &tandy_sound_program);
+
+    pio_sm_set_enabled(detection_pio, detection_sm, false);
+    pio_manager_unload(detection_pio, detection_sm, detection_offset, &tandy_detection_program);
 
     for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) {
         gpio_deinit(i);
